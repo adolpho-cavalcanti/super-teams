df669535e242322e320b61a68931596d66985619
Adolpho Cavalcanti <adolpho.nascimento@climatempo.com.br>
2023-12-29 18:12:38 -0300

    feat: add titulos mundiais e libertadores

--- src/app/(listTeams)/team/[slug]/page.tsx
+++ src/app/(listTeams)/team/[slug]/page.tsx
@@ -18,12 +18,37 @@ async function getTeamBySlug(slug: string): Promise<Team> {
   return team
 }
 
+const renderizarTitulos = (team: Team) => {
+  const rows: JSX.Element[] = []
+  let increment = 0
+
+  team.titulos.forEach((titulo) => {
+    for (let i = 1; i <= titulo.qtdTitulos; i++) {
+      increment++
+      rows.push(
+        <div key={increment}>
+          <Image
+            src={titulo.imagem}
+            className="group-hover:scale-105 transition-transform duration-500"
+            width={50}
+            height={50}
+            quality={100}
+            alt={titulo.nome}
+          />
+        </div>,
+      )
+    }
+  })
+
+  return rows
+}
+
 export default async function ProductPage({ params }: TeamSlugProps) {
   const team = await getTeamBySlug(params.slug)
 
   return (
     <div className="bg-[#f2f2f2] rounded-lg mt-12 p-12 relative grid max-h-[860px] grid-cols-3">
-      <div className="col-span-2 overflow-hidden">
+      <div className="col-span-1 overflow-hidden">
         <Image
           src={team.imagem}
           alt=""
@@ -33,7 +58,7 @@ export default async function ProductPage({ params }: TeamSlugProps) {
         />
       </div>
 
-      <div className="flex flex-col justify-center px-12">
+      <div className="col-span-2 flex flex-col justify-center px-12">
         <h1 className="text-3xl font-bold leading-tight">{team.nome}</h1>
 
         {/* <p className="mt-2 flex gap-2 leading-relaxed text-zinc-400">
@@ -41,22 +66,9 @@ export default async function ProductPage({ params }: TeamSlugProps) {
           Time do Rei Pelé e um dos maiores da história!
         </p> */}
 
-        <>
-          {/* {champions.map((champion) => { */}
-          {/* return ( */}
-          <div key={1} className="mt-8 flex items-center gap-3">
-            <Image
-              src="/trofeus/libertadores.png"
-              className="group-hover:scale-105 transition-transform duration-500"
-              width={50}
-              height={50}
-              quality={100}
-              alt=""
-            />
-          </div>
-          {/* ) */}
-          {/* })} */}
-        </>
+        <div className="mt-8 flex items-center gap-1">
+          {renderizarTitulos(team)}
+        </div>
 
         <button
           type="button"
