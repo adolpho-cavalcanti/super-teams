aa0e9962e9d473f5fff916960fa08e8afe803449
Adolpho Cavalcanti <adolpho.nascimento@climatempo.com.br>
2024-01-04 13:58:10 -0300

    feat: removendo localstorage

--- src/contexts/compare-team-context.tsx
+++ src/contexts/compare-team-context.tsx
@@ -18,30 +18,18 @@ interface CompareTeamContextType {
 const CompareTeamContext = createContext({} as CompareTeamContextType)
 
 export function CompareTeamProvider({ children }: { children: ReactNode }) {
-  const storedItems =
-    typeof window !== 'undefined'
-      ? JSON.parse(localStorage.getItem('teams') || 'null')
-      : null
-
   const [compareTeamItems, setCompareTeamItems] = useState<Team[]>([])
   const [maxLimitTeams, setMaxLimitTeams] = useState<boolean>(false)
 
   function addCompareTeam(team: Team) {
     if (compareTeamItems?.length < 2) {
       setCompareTeamItems((state) => {
-        if (typeof window !== 'undefined') {
-          localStorage.setItem('teams', JSON.stringify([...state, team]))
-        }
         return [...state, team]
       })
     } else {
       setMaxLimitTeams(true)
     }
   }
-
-  useEffect(() => {
-    setCompareTeamItems(storedItems)
-  }, [storedItems, compareTeamItems])
 
   return (
     <CompareTeamContext.Provider
